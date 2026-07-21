0f83c8dd779fc6e2ec7c61951aca55ccd5197b10
Chrishoppers <166908023+Chrishoppers@users.noreply.github.com>
2026-07-21 19:38:21 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -23,6 +23,7 @@ builder.Services.AddHttpClient();
 builder.Services.AddHttpContextAccessor();
 
 builder.Services.AddScoped<BrevoEmailService>();   // ⭐ Brevo 邮件服务
+builder.Services.AddScoped<EmailRateLimitService>();
 builder.Services.AddScoped<SvgCaptchaService>();
 builder.Services.AddScoped<RateLimitService>();
 
