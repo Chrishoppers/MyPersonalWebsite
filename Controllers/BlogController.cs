a928d340bf2c205d5f159b7cf502ed571ebc37e8
Chrishoppers <166908023+Chrishoppers@users.noreply.github.com>
2026-07-22 01:40:28 +0800

    Update BlogController.cs

--- Controllers/BlogController.cs
+++ Controllers/BlogController.cs
@@ -18,20 +18,12 @@ namespace MyPersonalWebsite.Controllers
             _dataSync = dataSync;
         }
 
-        // ============================================================
-        // 博客列表
-        // ============================================================
         public async Task<IActionResult> Index()
         {
             var blogs = await _dataSync.GetBlogsAsync();
 
             var userId = HttpContext.Session.GetInt32("UserId");
             var likedIds = new HashSet<int>();
-            // TODO: 从 DataSync 获取点赞数据
-            // if (userId.HasValue)
-            // {
-            //     likedIds = await _dataSync.GetUserLikedBlogIdsAsync(userId.Value);
-            // }
 
             ViewBag.LikedIds = likedIds;
             ViewBag.CurrentUserId = userId;
@@ -39,9 +31,6 @@ namespace MyPersonalWebsite.Controllers
             return View(blogs);
         }
 
-        // ============================================================
-        // 博客详情
-        // ============================================================
         public async Task<IActionResult> Details(int id)
         {
             var blog = await _dataSync.GetBlogByIdAsync(id);
@@ -52,11 +41,6 @@ namespace MyPersonalWebsite.Controllers
 
             var userId = HttpContext.Session.GetInt32("UserId");
             var isLiked = false;
-            // TODO: 从 DataSync 检查是否已点赞
-            // if (userId.HasValue)
-            // {
-            //     isLiked = await _dataSync.IsBlogLikedByUserAsync(id, userId.Value);
-            // }
 
             ViewBag.IsLiked = isLiked;
             ViewBag.CurrentUserId = userId;
@@ -64,9 +48,6 @@ namespace MyPersonalWebsite.Controllers
             return View(blog);
         }
 
-        // ============================================================
-        // 点赞/取消点赞
-        // ============================================================
         [HttpPost]
         public async Task<IActionResult> ToggleLike(int blogId)
         {
@@ -79,9 +60,6 @@ namespace MyPersonalWebsite.Controllers
             try
             {
                 // TODO: 实现博客点赞逻辑
-                // var result = await _dataSync.ToggleBlogLikeAsync(blogId, userId.Value);
-                // return Json(new { success = true, isLiked = result.IsLiked, likeCount = result.LikeCount, message = result.Message });
-
                 return Json(new { success = true, isLiked = true, likeCount = 1, message = "点赞成功" });
             }
             catch (Exception ex)
