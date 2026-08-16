5dc040111ade61adff4d3627722de669bdae2fb1
Chrishoppers <166908023+Chrishoppers@users.noreply.github.com>
2026-08-16 22:38:11 +0800

    Update BrevoEmailService.cs

--- Services/BrevoEmailService.cs
+++ Services/BrevoEmailService.cs
@@ -4,6 +4,7 @@ using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
 using System.Collections.Generic;
+using MyPersonalWebsite.Models;  // ⭐ 添加这一行
 
 namespace MyPersonalWebsite.Services
 {
